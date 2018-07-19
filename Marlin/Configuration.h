db2bf841398dfb093f795ca176af015969640db8
Alex Gaudio <adgaudio@gmail.com>
2018-07-19 00:49:02 -0400

    ..

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -700,7 +700,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER -20  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER -20  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.22   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -1.24   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED HOMING_FEEDRATE_XY
